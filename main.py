9cba12fae3f5bd267005b9285af75ea6f84319da
joaonegro <joao@lab804.com.br>
2015-11-11 14:57:28 -0200

    main function

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 __author__ = 'joao'
+
+from cptec_bots.text_search import PlaceTextFinder
+from cptec_bots.download import FileSave
+from image_analyser.img_analyser import GraphInterpreter
+
+if __name__ == '__main__':
+    _pks = [586, 2467, 4176, 4263]
+    # for _pk in _pks:
+    #     try:
+    #         print "comecando"
+    #         new_req = PlaceTextFinder(_pk)
+    #         file_obj = FileSave(new_req.find_city(), new_req.find_url())
+    #         file_obj.save_file()
+    #         print "terminado"
+    #     except Exception as e:
+    #         print e
+
+    humidity_arara = GraphInterpreter('araraquara/araraquara_11_11_2015.png',
+                                (0, 10),
+                                'rain')
+
+    print type(humidity_arara.np_image_matrix())
+    # humidity_arara.save_values()
