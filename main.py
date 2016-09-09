e845cab6ad003ed20b87bcf4a634b3987c93c532
joaoTrevizoli <joao@lab804.com.br>
2016-09-09 12:45:04 -0300

    report

--- main.py
+++ main.py
@@ -8,20 +8,34 @@ from cptec_crawling.cptec_bots.download import FileSave
 from cptec_crawling.image_analyser.img_analyser import GraphInterpreter
 
 if __name__ == '__main__':
-    _pks = [586, 2467, 4176, 4263]
-    for _pk in _pks:
-        try:
-            print "comecando"
-            new_req = PlaceTextFinder(_pk)
-            file_obj = FileSave(new_req.find_city(), new_req.find_url())
-            file_obj.save_file()
-            print "terminado"
-        except Exception as e:
-            print e
+    # _pks = [586, 2467, 4176, 4263, 2673]
+    # for _pk in _pks:
+    #     try:
+    #         print "comecando"
+    #         new_req = PlaceTextFinder(_pk)
+    #         file_obj = FileSave(new_req.find_city(), new_req.find_url())
+    #         file_obj.save_file()
+    #         print "terminado"
+    #     except Exception as e:
+    #         print e
 
-    humidity_arara = GraphInterpreter('araraquara/araraquara_11_11_2015.png',
+    araraquara = GraphInterpreter('iracemápolis/iracemápolis_13_11_2015.png',
                                 (0, 10),
                                 'rain')
 
-    print type(humidity_arara.np_image_matrix())
-    humidity_arara.save_values()
+
+
+    araraquara.save_values()
+
+    araraquara = GraphInterpreter('iracemápolis/iracemápolis_13_11_2015.png',
+                                (14, 38),
+                                'temp')
+
+    araraquara.save_values()
+
+    araraquara = GraphInterpreter('iracemápolis/iracemápolis_13_11_2015.png',
+                                (20, 100),
+                                'humidity')
+
+    araraquara.save_values()
+    # jaboticabal.save_values()
